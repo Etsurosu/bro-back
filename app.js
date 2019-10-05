bdae38337aba7c4a309624395db961e602eb453b
Pierre-Alexandre Monneau <pierre-alexandre.monneau@epitech.eu>
2019-10-05 05:02:26 +0200

    auth login/current/register

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const errorHandler = require('errorhandler');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+mongoose.promise = global.Promise;
+const isProduction = process.env.NODE_ENV === 'production';
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+if(!isProduction) {
+    app.use(errorHandler());
+}
+
+mongoose.connect('mongodb://localhost/brodb', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('debug', true);
+
+require('./models/Users');
+require('./config/passport');
+app.use(require('./routes'));
+
+if(!isProduction) {
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+
+        res.json({
+        errors: {
+            message: err.message,
+            error: err,
+        },
+        });
+    });
+}
 
 module.exports = app;
---
+++ routes/api/users.js
@@ -0,0 +1,86 @@
+const mongoose = require('mongoose');
+const passport = require('passport');
+const router = require('express').Router();
+const auth = require('../auth');
+const Users = mongoose.model('Users');
+
+router.post('/register', auth.optional, async (req, res, next) => {
+  const { body: { user } } = req;
+
+  if(!user.email) {
+    return res.status(400).json({
+      errors: {
+        email: 'is required',
+      },
+    });
+  }
+
+  if(!user.password) {
+    return res.status(400).json({
+      errors: {
+        password: 'is required',
+      },
+    });
+  }
+
+  const finalUser = new Users(user);
+
+  finalUser.setPassword(user.password);
+
+  return finalUser.save()
+    .then(() => res.json({ user: finalUser.toAuthJSON() })).catch(err => res.status(401).json({
+        errors: {
+          messagec: 'user already exist',
+        },
+      }));
+});
+
+router.post('/login', auth.optional, (req, res, next) => {
+  const { body: { user } } = req;
+
+  if(!user.email) {
+    return res.status(422).json({
+      errors: {
+        email: 'is required',
+      },
+    });
+  }
+
+  if(!user.password) {
+    return res.status(422).json({
+      errors: {
+        password: 'is required',
+      },
+    });
+  }
+
+  return passport.authenticate('local', { session: false }, (err, passportUser, info) => {
+    if(err) {
+      return next(err);
+    }
+
+    if(passportUser) {
+      const user = passportUser;
+      user.token = passportUser.generateJWT();
+
+      return res.json({ user: user.toAuthJSON() });
+    }
+
+    return res.status(400).send(info)
+  })(req, res, next);
+});
+
+router.get('/current', auth.required, (req, res, next) => {
+  const { payload: { id } } = req;
+
+  return Users.findById(id)
+    .then((user) => {
+      if(!user) {
+        return res.sendStatus(400);
+      }
+
+      return res.json({ user: user.toAuthJSON() });
+    });
+});
+
+module.exports = router;
